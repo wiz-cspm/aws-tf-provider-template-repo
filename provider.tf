0917665578bc45416e2ca3a4b3ccb327792a6586
Ravi Kumar <RjDev@users.noreply.github.com>
2023-03-10 19:06:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,25 @@
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    archive = "~> 1.3"
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
   region     = var.REGION
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
 
+
   default_tags {
     tags = {
       env         = var.env
-      provider    = var.provider
+      provider    = var.tf_provider
       platform    = var.platform
       application = var.application
       cost-center = var.cost_center
@@ -15,3 +28,4 @@ provider "aws" {
     }
   }
 }
+

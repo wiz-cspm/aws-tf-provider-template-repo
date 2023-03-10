3ee11fc6c5832083f95e806c0afea9b7ccae121e
RjDev <rjdevnic@gmail.com>
2023-03-10 16:55:55 +0530

    added default tags section in provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,17 @@
 provider "aws" {
-  region = "us-east-1"
-  access_key=var.AWS_ACCESS_KEY
-  secret_key=var.AWS_SECRET_KEY
+  region     = var.REGION
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+
+  default_tags {
+    tags = {
+      env         = var.env
+      provider    = var.provider
+      platform    = var.platform
+      application = var.application
+      cost-center = var.cost_center
+      owner       = var.owner
+      lob         = var.lob
+    }
+  }
 }
